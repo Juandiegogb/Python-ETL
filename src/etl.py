3c964a85d3bf1ab907ffa9dd54b4de02c5f1039a
Juandiegogb <juandiegar@gmail.com>
2024-12-14 20:06:19 -0500

    testing

--- src/etl.py
+++ src/etl.py
@@ -34,6 +34,14 @@ def extract(engine: Engine) -> pd.DataFrame:
         connection.close()
 
 
+def extractCSV() -> pd.DataFrame:
+    df = pd.read_csv("transactions.csv")
+    saveFile(df.head(5), "head")
+    df.info()
+    description_df = df.describe()
+    return description_df
+
+
 def transform(df: DataFrame):
     # print(filtered_countries.head())
     country_counts = df.groupby("Country").size().reset_index(name="Record Count")
@@ -66,8 +74,10 @@ def transform(df: DataFrame):
 
 
 def main():
-    df = extract(engine)
-    transform(df)
+    df = extractCSV()
+    print(df)
+    # df = extract(engine)
+    # transform(df)
 
 
 main()
