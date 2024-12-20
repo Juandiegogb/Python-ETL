35c293fcb4493953e2bf84a812a9f30e8803fae8
Juan Diego <juandiegar@gmail.com>
2024-12-20 16:19:13 -0500

    fixing bugs after architecture change

--- src/etl/db.py
+++ src/etl/db.py
@@ -14,7 +14,7 @@ class DB:
     df: DataFrame
 
     def __init__(self, variableName: str, origin=True):
-        origin = origin
+        self.origin = origin
         dbInfo = getDBInfo(variableName)
         user = dbInfo["user"]
         password = dbInfo["password"]
@@ -27,12 +27,13 @@ class DB:
             "postgres": f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}",
             "mysql": f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}",
             "mssql": f"mssql+pyodbc://{user}:{password}@{host}:{port}/{database}?driver=ODBC+Driver+17+for+SQL+Server",
-            "oracle": f"oracle+cx_oracle://{user}:{password}@{host}:{port}/{database}",
+            "oracle": f"oracle+cx_oracle://{user}:{password}@{host}/{database}",
         }
         self.connectionURL = dbURL[engine]
 
         try:
             newEngine: Engine = create_engine(self.connectionURL)
+
             self.connection = newEngine.connect()
             print("DB connected")
         except Exception as e:
--- src/main.py
+++ src/main.py
@@ -7,13 +7,12 @@ def main():
     originDB = DB("DB_ORIGIN")
     query = "SELECT * FROM public.global_health_statistics"
     originDB.extract(query)
-
+    
     newTransform = Transform(originDB.df)
     newTransform.exploreData()
     transformedDataframes = newTransform.transformData()
 
-    destinyDB = DB("DB_DESTINY")
+    destinyDB = DB("DB_DESTINY", False)
     destinyDB.load(transformedDataframes)
-
 
 main()
